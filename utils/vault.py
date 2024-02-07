9d85d16ef1618a953b4930a3b864be22f7dd1bf7
Krystian Gaus <krystiangaus@gmail.com>
2024-02-07 11:17:29 +0000

    renamed classes

--- utils/vault.py
+++ utils/vault.py
@@ -1,4 +1,4 @@
-class Trove:
+class Vault:
     def __init__(self, coll_price: float, coll_balance: float, min_coll_ratio: float, loan_value: float):
         if coll_price < 0 or coll_balance < 0 or loan_value < 0:
             raise Exception("Numbers below zero forbidden")
@@ -47,7 +47,7 @@ class Trove:
             updated_loan_value = self.loan_value + delta_loan_value
             # negative values are allowed
             if updated_coll_balance < 0 or updated_loan_value < 0:
-                raise Exception("Cannot extract more from the trove than it holds")
+                raise Exception("Cannot extract more from vault than it holds")
             updated_coll_value = updated_coll_balance * self.__latest_coll_price
             min_coll_ratio_criterion = updated_coll_value >= self.min_coll_ratio * updated_loan_value
             if min_coll_ratio_criterion:
